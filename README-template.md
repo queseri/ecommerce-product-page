d916e28d8e6a0aaa2837f65e91c94249666e0492
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-02-20 17:37:06 +0200

    netlify deploy and header layout

--- README-template.md
+++ README-template.md
@@ -45,7 +45,7 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live site URL: [ecommerce product page](https://chamu-ecommerce-product-page.netlify.app)
 - Live Site URL: [Add live site URL here](https://your-live-site-url.com)
 
 ## My process
---
+++ src/header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import hamburger from '../images/icon-menu.svg'
+import cart from '../images/icon-cart.svg'
+import profile from '../images/image-avatar.png'
+import logo from '../images/logo.svg'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="menu-control">
+            <button className="open-modal-nav">
+                <img src={hamburger} alt="" />
+            </button>
+            <div className="logo-container">
+                <img src={logo} alt="" />
+            </div>
+        </div>
+        <div className="product-summary">
+            <div className="cart-summary">
+                <img src={cart} alt="" />
+            </div>
+            <div className="profile">
+                <img src={profile} alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
